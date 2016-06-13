0cd683cfa87fb5700be3f114de54e52786752cac
rocky1001 <rocky.qi@qq.com>
2016-06-13 08:58:37 +0800

    add kylin 'join support by

--- pykylin/cursor.py
+++ pykylin/cursor.py
@@ -1,12 +1,18 @@
 from __future__ import absolute_import
 
+import re
+
 from dateutil import parser
 
 from .errors import Error
 from .log import logger
 
-class Cursor(object):
+rep_sql_regx = re.compile("('\$\|.*\|\$')")
+limit_sql_regx = re.compile("(limit\s*[0-9]{1,6})")
+join_on_sql_regx = re.compile("join.*on.*\s*where", re.DOTALL)
 
+
+class Cursor(object):
     def __init__(self, connection):
         self.connection = connection
         self._arraysize = 1
@@ -17,15 +23,51 @@ class Cursor(object):
         self.fetched_rows = 0
 
     def callproc(self):
-        raise('Stored procedures not supported in Kylin')
+        raise ('Stored procedures not supported in Kylin')
 
     def close(self):
         logger.debug('Cursor close called')
 
+    @staticmethod
+    def trans_sql_for_kylin(sql_str):
+        sql_str = sql_str.lower()
+        rep_sql_list = rep_sql_regx.findall(sql_str)
+        if not rep_sql_list:
+            return sql_str
+        transformed_sql = ''
+        rep_sql_dict = dict()
+        rep_sql_set = set()
+        for rep_sql in rep_sql_list:
+            rep_sql_dict[rep_sql] = rep_sql.split('|')[-2]
+            rep_sql_set.add(rep_sql.split('|')[1])
+
+        # step 1, replace all the const str to data name
+        for rep_sql, rep_value in rep_sql_dict.items():
+            transformed_sql = sql_str.replace(rep_sql, rep_value)
+
+        # step 2, replace limit number from sub-sql
+        limit_sql_list = limit_sql_regx.findall(transformed_sql)
+        if len(limit_sql_list) == 2:
+            transformed_sql = transformed_sql.replace(limit_sql_list[1], limit_sql_list[0])
+
+        # step 3, delete 'join on' sql
+        join_on_sql = join_on_sql_regx.findall(transformed_sql)
+        if join_on_sql:
+            transformed_sql = transformed_sql.replace(join_on_sql[0], 'where')
+
+        # step 4, replace 'where' with rep_sql(inner join)
+        for rep_sql in rep_sql_set:
+            transformed_sql = transformed_sql.replace('where', '%s %s' % (rep_sql, 'where'))
+
+        return transformed_sql
+
     def execute(self, operation, parameters={}, acceptPartial=True, limit=None, offset=0):
-        sql = operation % parameters
+        if parameters:
+            sql = operation % parameters
+        else:
+            sql = operation
         data = {
-            'sql': sql,
+            'sql': self.trans_sql_for_kylin(sql.lower()),
             'offset': offset,
             'limit': limit or self.connection.limit,
             'acceptPartial': acceptPartial,
